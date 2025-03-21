9a411156fb66c9e5c6eed0ab48bc22da9ebdac8c
NiceKid <154478981+NiceKid69@users.noreply.github.com>
2025-03-21 16:25:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.137.5';
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('solarSystem') });
